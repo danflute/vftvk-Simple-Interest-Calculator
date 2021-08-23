e86baf774d1d028526fead0c2321149d7d6952c2
danflute <81410938+danflute@users.noreply.github.com>
2021-08-23 12:11:04 +0800

    Update script.js
    edX Project JS

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
 function compute()
 {
+    //create variable and assign value
     p = document.getElementById("principal").value;
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
     
+    //calculate interest
+    var interest = principal * years * rate /100;
+    
+    //No of Years conversion
+    var year = new Date().getFullYear()+parseInt(years);
+    
+    //set innerHTML for var result
+    document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>"
 }
-        
+
+<select id="rate" onchange="updateRate()"></select>
+
+//reads value of range slider and displays
+function updateRate() {
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
+}
